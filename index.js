e50a46724187875e4d98d83ea7ded5ab428bcc46
Kit <jajones0707@gmail.com>
2023-04-27 09:06:08 -0400

    nya

--- index.js
+++ index.js
@@ -1,53 +1,55 @@
-async function get_ip_address() {
-  const response = await fetch('https://api.ipify.org');
-  const ip = await response.text();
-  return ip;
-}
-function findinfo() {
-  const history = [];
-  const emailCounts = {};
-
-  const keywords = ['porn', 'xxx', 'hentai', 'yiff', 'e621'];
-  const browserHistory = window.history;
-
-  // Iterate through the user's browsing history
-  for (let i = 0; i < browserHistory.length; i++) {
-    const url = browserHistory[i];
-
-    // Check if the URL contains any of the specified keywords
-    if (keywords.some(keyword => url.includes(keyword))) {
-      history.push(url);
-    }
-
-    // Extract email addresses from the URL using a regular expression
-    const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
-    const emails = url.match(emailRegex);
-    if (emails) {
-      emails.forEach(email => {
-        const emailLowercase = email.toLowerCase();
-        if (emailLowercase in emailCounts) {
-          emailCounts[emailLowercase]++;
-        } else {
-          emailCounts[emailLowercase] = 1;
-        }
-      });
-    }
-  }
-
-  // Find the email address with the highest occurrence count
-  let maxCount = 0;
-  let maxEmail = '';
-  for (const email in emailCounts) {
-    if (emailCounts[email] > maxCount) {
-      maxCount = emailCounts[email];
-      maxEmail = email;
-    }
-  }
-
-  // Return the user's browsing history and the email address with the highest occurrence count
-  return { history, email: maxEmail };
+// Function to get the user's IP address
+function getIPAddress() {
+  return fetch('https://api.ipify.org/?format=json')
+    .then(response => response.json())
+    .then(data => data.ip);
 }
 
-alert('hey')
-alert(findinfo())
-console.log(findinfo())
+// Function to get the user's current location
+function getCurrentLocation() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      position => resolve({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      }),
+      error => reject(error)
+    );
+  });
+}
+
+// Function to get the address for a given latitude and longitude using reverse geocoding
+async function getAddress(latitude, longitude) {
+  const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`);
+  const data = await response.json();
+  return data.display_name;
+}
+
+// Function to display the IP address, geolocation, and address on the page
+async function showResult() {
+  try {
+    const ip = await getIPAddress();
+    const location = await getCurrentLocation();
+    const address = await getAddress(location.latitude, location.longitude);
+
+    const result = document.getElementById('result');
+    result.innerHTML = `
+      <p>Your IP address is: ${ip}</p>
+      <p>Your geolocation is:</p>
+      <ul>
+        <li>Latitude: ${location.latitude}</li>
+        <li>Longitude: ${location.longitude}</li>
+      </ul>
+      <p>Your current address is:</p>
+      <p>${address}</p>
+    `;
+  } catch (error) {
+    console.error(error);
+    const result = document.getElementById('result');
+    result.innerHTML = `
+      <p>Unable to retrieve your current location.</p>
+    `;
+  }
+}
+
+showResult();
