00ec6351a8a497724ff19cfb99105cb9e8056093
Kit <jajones0707@gmail.com>
2023-04-29 14:11:47 -0400

    lets GOOOO

--- twitter/slides.js
+++ twitter/slides.js
@@ -1,19 +1,22 @@
-// define the path to the folder containing the images
-    const imagePath = "images/";
-    
-    // retrieve all the images in the folder
-    fetch(imagePath)
-      .then(response => response.text())
-      .then(text => {
-        const parser = new DOMParser();
-        const html = parser.parseFromString(text, "text/html");
-        const images = html.querySelectorAll("a[href$='.jpg'], a[href$='.png'], a[href$='.gif']");
-        
-        // create the slideshow
-        const slideshow = document.getElementById("slideshow");
-        let currentIndex = 0;
-        setInterval(() => {
-          slideshow.style.backgroundImage = `url(${imagePath}${images[currentIndex].getAttribute("href")})`;
-          currentIndex = (currentIndex + 1) % images.length;
-        }, 5000);
-      });
+const slideshow = document.getElementById("slideshow");
+let scrollAmount = 0;
+const scrollStep = 150;
+let slideTimer;
+
+function startTimer() {
+  slideTimer = setInterval(() => {
+    scrollAmount += scrollStep;
+    slideshow.scroll({
+      top: 0,
+      left: scrollAmount,
+      behavior: "smooth"
+    });
+    if (scrollAmount >= slideshow.scrollWidth - slideshow.clientWidth) {
+      clearInterval(slideTimer);
+      scrollAmount = 0;
+      startTimer();
+    }
+  }, 3000);
+}
+
+startTimer();
